add05fe1a5b2dc8f196bab12c3316fdc3b547eb3
shenjixiaodao <972249598@qq.com>
2018-10-19 18:09:06 +0800

    ui-server

--- ui-factory/src/api/common.js
+++ ui-factory/src/api/common.js
@@ -1,9 +1,24 @@
 import request from '@/utils/request'
+import { encryptPwd } from '@/utils/encrypt'
 
 export function queryByDentureId(dentureId) {
   return request({
     url: '/manufacture/queryByDentureId',
     method: 'get',
     params: { dentureId: dentureId }
+  })
+}
+
+export function modifyPwd(srcPwd, dstPwd) {
+  return request({
+    url: '/factoryUser/modifyPwd',
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: {
+      srcPwd: encryptPwd(srcPwd),
+      dstPwd: encryptPwd(dstPwd)
+    }
   })
 }
