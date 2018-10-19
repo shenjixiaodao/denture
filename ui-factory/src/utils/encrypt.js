719d2bda8c4b2c8365408c1b3d9f00c53595ac3c
shenjixiaodao <972249598@qq.com>
2018-10-19 18:10:39 +0800

    ui-server

---
+++ ui-factory/src/utils/encrypt.js
@@ -0,0 +1,9 @@
+import { JSEncrypt } from 'jsencrypt'
+
+const PublicKey = 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtJ9qG8bnsWKCpeGHZwjey74C93KAHMMHF6MwJdv2cv0GXbRjLq212ZbnA5sgqBb85lZy+5OokHVj96YEEPIInIw9Kk5qwwzX4j5wZcQEKelnt+/wKEQwg1FbJFjUUp6FCdYCVme9RUfHo1G7sNTq9MfwVTHqK7J49fQ1FTJEq9/DXi0ajdHCRduOYtNUrfbYAG7p8ngTJnbBfdpfSAcT2tNNbKQy27cjYcEspUn8j/WKtE8ALwebluu1En5VQA69C4cQvhVaRDjbyObnaW/8eWb37A+jhv7wEDnAzsiFXM1c1SmfRc7eoHHPNAKBnhw1kt0hLNMV2LYuCgkgRm/V3QIDAQAB'
+const encryptor = new JSEncrypt()
+encryptor.setPublicKey(PublicKey)
+
+export function encryptPwd(password) {
+  return encryptor.encrypt(password)
+}
