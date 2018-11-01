f5348807d0780ecc42deae87b24cf72d586f2b77
you.li <you.lee@royole.com>
2018-11-01 10:58:26 +0800

    factory

--- ui-factory/src/utils/request.js
+++ ui-factory/src/utils/request.js
@@ -7,7 +7,6 @@ import * as QS from 'qs'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.BASE_API, // api 的 base_url
-  timeout: 5000, // request timeout
   withCredentials: true
 })
 
